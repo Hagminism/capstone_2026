bded59934b553d709c152e2f185f03642ccf14cf
Hagmin.Lee <iilhm2001@naver.com>
2026-03-23 14:26:58 +0900

    feat: GetIt을 활용한 의존성 주입(DI) 초기 설정 추가
    - lib/di/di_setup.dart: GetIt 패키지를 사용하여 의존성 주입 환경을 설정하는 `diSetup` 함수 정의.
    - ViewModel 등록: `SignInViewModel` 및 `SignUpViewModel`을 LazySingleton으로 등록하여 전역적인 의존성 관리 체계 구축.

---
+++ lib/di/di_setup.dart
@@ -0,0 +1,15 @@
+import 'package:capstone_2026/feature/sign_in/presentation/sign_in_view_model.dart';
+import 'package:capstone_2026/feature/sign_up/presentation/sign_up_view_model.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt getIt = GetIt.instance;
+
+void diSetup() {
+  // ViewModel
+  getIt.registerLazySingleton<SignInViewModel>(
+    () => SignInViewModel(),
+  );
+  getIt.registerLazySingleton<SignUpViewModel>(
+    () => SignUpViewModel(),
+  );
+}
