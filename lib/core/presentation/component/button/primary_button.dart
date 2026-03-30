c74cc86a58321a53f012d9a97321d21177f38605
Hagminism <iilhm110629@gmail.com>
2026-03-30 10:50:22 +0900

    feat: 공통 UI 컴포넌트 PrimaryButton 추가
    - primary_button.dart: 앱 전반에서 범용적으로 사용할 수 있는 `PrimaryButton` 위젯 구현.
    - 기능 정의: 필수 속성인 `text`와 `onTap`을 포함하여 `textColor`, `fontSize`, `borderRadius`, `height` 등 스타일을 커스터마이징할 수 있는 옵션 제공.
    - 스타일 및 효과: `AppColors.primary`를 기본 배경색으로 사용하며, `InkWell`을 활용하여 터치 피드백(Ripple effect)이 포함된 버튼 디자인 적용.

---
+++ lib/core/presentation/component/button/primary_button.dart
@@ -0,0 +1,53 @@
+import 'package:capstone_2026/ui/app_colors.dart';
+import 'package:capstone_2026/ui/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final void Function() onTap;
+  final Color? textColor;
+  final double? fontSize;
+  final double? borderRadius;
+  final double? height;
+
+  const PrimaryButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+    this.textColor,
+    this.fontSize,
+    this.borderRadius,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius:
+            BorderRadius.circular((borderRadius != null) ? borderRadius! : 12),
+        child: Ink(
+          height: (height != null) ? height! : 56,
+          decoration: BoxDecoration(
+            color: AppColors.primary,
+            borderRadius: BorderRadius.circular(
+                (borderRadius != null) ? borderRadius! : 12),
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: AppTextStyles.subtitle.copyWith(
+                fontWeight: FontWeight.w700,
+                fontSize: (fontSize != null) ? fontSize! : null,
+                color:
+                    (textColor != null) ? textColor : AppColors.textOnPrimary,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
