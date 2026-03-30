5f8127c7a05b554300bc7e81eb13dedf083557f1
Hagminism <iilhm110629@gmail.com>
2026-03-30 10:55:37 +0900

    feat: 의존성 주입(DI) 설정에 AuthRepository 및 Firebase 관련 객체 추가
    - di_setup.dart: `FirebaseAuth`, `GoogleSignIn` 인스턴스를 LazySingleton으로 등록.
    - di_setup.dart: `AuthRepositoryImpl`을 `AuthRepository` 인터페이스의 구현체로 등록하고, 필요한 의존성(`FirebaseAuth`, `GoogleSignIn`)을 주입하도록 설정.
    - di_setup.dart: `SignInViewModel` 등록 시 `AuthRepository`를 생성자 파라미터로 주입하도록 수정.
    - di_setup.dart: `SignInViewModel` 임포트 경로를 파일 구조 변경에 맞춰 업데이트.

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -1,15 +1,19 @@
+import 'package:capstone_2026/core/data/repository/auth_repository_impl.dart';
+import 'package:capstone_2026/core/domain/repository/auth_repository.dart';
 import 'package:capstone_2026/feature/find_password/presentation/find_password_view_model.dart';
-import 'package:capstone_2026/feature/sign_in/presentation/sign_in_view_model.dart';
+import 'package:capstone_2026/feature/sign_in/presentation/screen/sign_in_view_model.dart';
 import 'package:capstone_2026/feature/sign_up_partner/presentation/sign_up_partner_view_model.dart';
 import 'package:capstone_2026/feature/sign_up/presentation/sign_up_view_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 GetIt getIt = GetIt.instance;
 
 void diSetup() {
   // ViewModel
   getIt.registerFactory<SignInViewModel>(
-    () => SignInViewModel(),
+    () => SignInViewModel(authRepository: getIt<AuthRepository>()),
   );
   getIt.registerFactory<FindPasswordViewModel>(
     () => FindPasswordViewModel(),
@@ -19,5 +23,21 @@ void diSetup() {
   );
   getIt.registerFactory<SignUpPartnerViewModel>(
     () => SignUpPartnerViewModel(),
+  );
+
+  // Repository
+  getIt.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(
+      firebaseAuth: getIt<FirebaseAuth>(),
+      googleSignIn: getIt<GoogleSignIn>(),
+    ),
+  );
+
+  // Auth
+  getIt.registerLazySingleton<GoogleSignIn>(
+    () => GoogleSignIn.instance,
+  );
+  getIt.registerLazySingleton<FirebaseAuth>(
+    () => FirebaseAuth.instance,
   );
 }
