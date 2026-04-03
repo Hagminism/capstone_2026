b59c0e74e7a0f9ee6ddf8e137e76268ca0658a80
Hagminism <iilhm110629@gmail.com>
2026-04-03 19:27:46 +0900

    feat: MyPageScreenRoot 내 ViewModel 주입 및 프로필 데이터 로드 로직 추가
    - `MyPageScreenRoot`: `StatelessWidget`에서 `StatefulWidget`으로 변경하고 `MyPageViewModel`을 외부에서 주입받도록 수정.
    - 데이터 초기화: `initState` 생명주기 메서드에서 `viewModel.fetchProfile()`을 호출하여 초기 프로필 정보 로드.
    - 상태 관리: `ListenableBuilder`를 적용하여 ViewModel의 상태 변화에 따라 화면이 리빌드되도록 구현.
    - `MyPageScreen`: 주입받은 ViewModel의 상태(`state`)를 하위 위젯으로 전달.

--- lib/feature/my_page/settings/presentation/screen/my_page_screen_root.dart
+++ lib/feature/my_page/settings/presentation/screen/my_page_screen_root.dart
@@ -1,45 +1,68 @@
 import 'package:capstone_2026/core/routing/routes.dart';
 import 'package:capstone_2026/feature/my_page/settings/presentation/screen/my_page_action.dart';
 import 'package:capstone_2026/feature/my_page/settings/presentation/screen/my_page_screen.dart';
+import 'package:capstone_2026/feature/my_page/settings/presentation/screen/my_page_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class MyPageScreenRoot extends StatelessWidget {
-  const MyPageScreenRoot({super.key});
+class MyPageScreenRoot extends StatefulWidget {
+  final MyPageViewModel viewModel;
+
+  const MyPageScreenRoot({
+    super.key,
+    required this.viewModel,
+  });
+
+  @override
+  State<MyPageScreenRoot> createState() => _MyPageScreenRootState();
+}
+
+class _MyPageScreenRootState extends State<MyPageScreenRoot> {
+  @override
+  void initState() {
+    super.initState();
+    widget.viewModel.fetchProfile();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MyPageScreen(
-      onAction: (action) {
-        switch (action) {
-          case ViewNotifications():
-            context.push('${Routes.myPage}/${Routes.notifications}');
-            break;
-          case EditProfile():
-            context.push('${Routes.myPage}/${Routes.profileEdit}');
-            break;
-          case ViewReservationHistory():
-            context.push('${Routes.myPage}/${Routes.reservationHistory}');
-            break;
-          case ViewReviewHistory():
-            context.push('${Routes.myPage}/${Routes.reviewHistory}');
-            break;
-          case TapAccountSettings():
-            context.push('${Routes.myPage}/${Routes.accountSettings}');
-            break;
-          case TapNotificationSettings():
-            context.push('${Routes.myPage}/${Routes.notificationSettings}');
-            break;
-          case TapInquiry():
-            context.push('${Routes.myPage}/${Routes.inquiry}');
-            break;
-          case ViewNotices():
-            context.push('${Routes.myPage}/${Routes.notices}');
-            break;
-          case ViewTerms():
-            context.push('${Routes.myPage}/${Routes.terms}');
-            break;
-        }
+    return ListenableBuilder(
+      listenable: widget.viewModel,
+      builder: (context, child) {
+        return MyPageScreen(
+          state: widget.viewModel.state,
+          onAction: (action) {
+            switch (action) {
+              case ViewNotifications():
+                context.push('${Routes.myPage}/${Routes.notifications}');
+                break;
+              case EditProfile():
+                context.push('${Routes.myPage}/${Routes.profileEdit}');
+                break;
+              case ViewReservationHistory():
+                context.push('${Routes.myPage}/${Routes.reservationHistory}');
+                break;
+              case ViewReviewHistory():
+                context.push('${Routes.myPage}/${Routes.reviewHistory}');
+                break;
+              case TapAccountSettings():
+                context.push('${Routes.myPage}/${Routes.accountSettings}');
+                break;
+              case TapNotificationSettings():
+                context.push('${Routes.myPage}/${Routes.notificationSettings}');
+                break;
+              case TapInquiry():
+                context.push('${Routes.myPage}/${Routes.inquiry}');
+                break;
+              case ViewNotices():
+                context.push('${Routes.myPage}/${Routes.notices}');
+                break;
+              case ViewTerms():
+                context.push('${Routes.myPage}/${Routes.terms}');
+                break;
+            }
+          },
+        );
       },
     );
   }
